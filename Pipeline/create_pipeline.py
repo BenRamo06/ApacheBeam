2559d8ee232a7ab9d6a8a8ab963bc8ab23a05eec
Benito Ramos <daniel.ramos06sql@gmail.com>
2021-09-11 00:42:58 -0500

    Update create pipeline

--- Pipeline/create_pipeline.py
+++ Pipeline/create_pipeline.py
@@ -1,5 +1,7 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 
+
+# We create a pipeline with Apache Beam without parameters
 with beam.Pipeline() as pipeline:
     pass
