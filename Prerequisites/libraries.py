88e8cdcf1bc28d54f5322aba39f3c1533702817b
BenRamo06 <daniel.ramos06sql@gmail.com>
2021-09-11 00:06:01 -0500

    Add libraries code

---
+++ Prerequisites/libraries.py
@@ -0,0 +1,18 @@
+import os
+
+# Run and print a shell command.
+def run(cmd):
+  os.system(cmd)
+  print('Command finished >> {}'.format(cmd))
+
+# Update and upgrade the system before installing anything else.
+run('apt-get update')
+run('apt-get upgrade')
+
+# Install apache-beam 
+run('pip install apache-beam')
+
+# Install libraries to GCP
+#run('pip install apache-beam[gcp]')
+#run('pip install apache-beam[interactive]')
+#run('pip install google-apitools')
