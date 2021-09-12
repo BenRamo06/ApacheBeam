d6c93aa73d7606e8c4edee17bf18c440bfab7952
Benito Ramos <daniel.ramos06sql@gmail.com>
2021-09-12 13:22:41 -0500

    Update files

--- Pipeline/02.-Create_PCollection.py
+++ Pipeline/02.-Create_PCollection.py
@@ -1,13 +1,12 @@
 import apache_beam as beam
 
+
 # Create Pipeline without options
-pipeline = beam.Pipeline()
+with beam.Pipeline() as pipeline:
 
 # Create PCollection with a list
 # We use function Map with print and We see the result
-(pipeline | 'Create PCollection' >> beam.Create([(1,10.3,'EUA'),
+    (pipeline | 'Create PCollection' >> beam.Create([(1,10.3,'EUA'),
                                                  (1,10.3,'MEX')])
-          | 'Print'              >> beam.Map(print))
+              | 'Print'              >> beam.Map(print))
 
-# Execute Pipeline
-pipeline.run().wait_until_finish()
