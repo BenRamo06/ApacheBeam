364874a15f15a323e989f20f06c1872944b70fa8
Benito Ramos <daniel.ramos06sql@gmail.com>
2021-09-12 13:31:11 -0500

    Update file

--- Pipeline/02.-Create_PCollection.py
+++ Pipeline/02.-Create_PCollection.py
@@ -7,6 +7,6 @@ with beam.Pipeline() as pipeline:
 # Create PCollection with a list
 # We use function Map with print and We see the result
     (pipeline | 'Create PCollection' >> beam.Create([(1,10.3,'EUA'),
-                                                 (1,10.3,'MEX')])
+                                                     (1,10.3,'MEX')])
               | 'Print'              >> beam.Map(print))
 
