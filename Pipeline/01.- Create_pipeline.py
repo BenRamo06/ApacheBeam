98a02df246c85c3d8f861896f85ff15d830413a1
Benito Ramos <daniel.ramos06sql@gmail.com>
2021-09-12 11:58:43 -0500

    Create pipeline two forms

--- Pipeline/01.- Create_pipeline.py
+++ Pipeline/01.- Create_pipeline.py
@@ -3,6 +3,17 @@ import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 
 
-# We create a pipeline with Apache Beam without parameters
+# We can create a pipeline two forms:
+
+# 1) We can use with form for create a Pipeline, and it will execute automatically
+
 with beam.Pipeline() as pipeline:
     pass
+
+
+# 2) We can use variable form for create a Pipeline 
+#    but we need to execute the pipeline manually with sentecen run
+
+pipeline = beam.Pipeline()
+
+pipeline.run().wait_until_finish()
