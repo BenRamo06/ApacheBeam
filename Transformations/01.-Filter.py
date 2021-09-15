d899fd7decc66708cff9ba42ddbc61ccfcb07821
Benito Ramos <daniel.ramos06sql@gmail.com>
2021-09-15 10:17:19 -0500

    Transformation update

--- Transformations/01.-Filter.py
+++ Transformations/01.-Filter.py
@@ -1,6 +1,5 @@
 import apache_beam as beam
 
-
 def valid_amount(row, max):
     amount = float(row[3])
     if amount >= max:
@@ -12,7 +11,12 @@ with beam.Pipeline() as pipeline:
     read_file  = (pipeline | 'Read File' >> beam.io.ReadFromText('inputs/InfoDataflow', skip_header_lines = 1)
                            | 'Split' >> beam.Map(lambda x: x.split(',')))
 
-    #valid_rows = read_file | 'Amount grather than 0' >> beam.Filter(lambda x: float(x[3]) > 0)
+    # We use 
+    valid_rows = read_file | 'Amount grather than 0' >> beam.Filter(lambda x: float(x[3]) > 0)
+    
+    
+    #
     valid_rows = read_file | 'Amount grather than 0' >> beam.Filter(valid_amount, max = 1469)
 
     print_data = valid_rows | 'Print' >> beam.Map(print)
+    
