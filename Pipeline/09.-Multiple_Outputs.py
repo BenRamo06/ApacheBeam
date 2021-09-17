afbd38dc3f3c3ce431231afc5f8f15ef203542a6
Benito Ramos <daniel.ramos06sql@gmail.com>
2021-09-16 22:42:45 -0500

    Multiples Outputs

--- Pipeline/09.-Multiple_Outputs.py
+++ Pipeline/09.-Multiple_Outputs.py
@@ -2,7 +2,6 @@ from os import read
 import apache_beam as beam
 
 def filters(row):
-
     try:
         row[3] = float(row[3])        
     except:
@@ -25,13 +24,13 @@ class convert (beam.DoFn):
 
 with beam.Pipeline() as pipeline:
 
-    read_file = pipeline | 'Read'        >> beam.io.ReadFromText(file_pattern = 'inputs/InfoDataflow', skip_header_lines = 1, )
+    read_file = pipeline   | 'Read'        >> beam.io.ReadFromText(file_pattern = 'inputs/InfoDataflow', skip_header_lines = 1)
 
-    divide = read_file   | 'Split data'  >> beam.Map(lambda x: x.split(','))
+    divide    = read_file  | 'Split data'  >> beam.Map(lambda x: x.split(','))
     
-    filters = divide     | 'Class'       >> beam.Filter(filters)
+    filters   = divide     | 'Filter data' >> beam.Filter(filters)
 
-    valids, fails = (filters | 'Convert' >> beam.ParDo(convert(header = 'id,date,amount1,amount2,amount3,amount4,target')).with_outputs('valids', 'fails'))
+    valids, fails = filters | 'Valid and Fails data ' >> beam.ParDo(convert(header = 'id,date,amount1,amount2,amount3,amount4,target')).with_outputs('valids', 'fails')
 
     export_fails  = fails  | 'Export Fails'  >> beam.io.WriteToText(file_path_prefix = 'outputs/TextFails', file_name_suffix = '.txt' )
 
