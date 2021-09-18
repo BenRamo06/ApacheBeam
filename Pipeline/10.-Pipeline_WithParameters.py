6186b4d6736b2ec513bbb884680846d90313ac00
Benito Ramos <daniel.ramos06sql@gmail.com>
2021-09-17 20:36:30 -0500

    Code Pipeline with parameters

---
+++ Pipeline/10.-Pipeline_WithParameters.py
@@ -0,0 +1,39 @@
+import argparse
+import apache_beam as beam
+
+from sys import argv
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.transforms.core import Map
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--input_files',
+                    required = False,
+                    default = '',
+                    help = 'Path files to process')
+
+parser.add_argument('--output_files',
+                    required = False,
+                    default = '',
+                    help = 'Path files to process')
+
+args_cmd, args_beam = parser.parse_known_args()
+
+
+with beam.Pipeline(options = PipelineOptions(args_beam)) as pipeline:
+
+    read_file = pipeline | 'Read' >> beam.io.ReadFromText(file_pattern = args_cmd.input_files, skip_header_lines = 1)
+
+    filter = read_file | 'Amount grater or equal than 0' >> beam.Filter(lambda x : float(x.split(',')[3]) > 0)
+
+    export = filter | 'Export data' >> beam.io.WriteToText(file_path_prefix = str(args_cmd.output_files).split('.')[0],
+                                                           file_name_suffix = '.' + str(args_cmd.output_files).split('.')[1])
+
+
+# Execution
+
+# python3 Pipeline/10.-Pipeline_WithParameters.py \
+# --input_files "inputs/InfoDataflow" \
+# --output_files "outputs/Output.txt" \
+# --runner DirectRunner
