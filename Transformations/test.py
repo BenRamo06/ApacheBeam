53aabbe93cddb650eb2486491183526d467466e7
Benito Ramos <daniel.ramos06sql@gmail.com>
2021-09-18 00:10:34 -0500

    Add files

--- Transformations/test.py
+++ Transformations/test.py
@@ -1,5 +1,12 @@
-lista = ["1","2","3","4","5"]
+import apache_beam as beam
 
 
+with beam.Pipeline() as pipeline:
+    
+    read_file = (pipeline | 'Read File' >> beam.io.ReadFromText('inputs/InfoDataflow', skip_header_lines = 1)
+                          | 'Divide' >> beam.Map(lambda x: (x.split(',')[0],float(x.split(',')[2])))
+                          | 'Count' >> beam.CombinePerKey(sum)
+                )
+                          
 
-print((lista[1],lista[2]+',' +lista[3]))
+    print_data = read_file | 'Print' >> beam.Map(print)
