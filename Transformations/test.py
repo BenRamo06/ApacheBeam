47da8a6bfd7aade8f05c1343f8f1e7063ba51f7f
Benito Ramos <daniel.ramos06sql@gmail.com>
2021-09-21 22:03:27 -0500

    code test

--- Transformations/test.py
+++ Transformations/test.py
@@ -1,12 +1,44 @@
 import apache_beam as beam
+import argparse
+import time
+from datetime import datetime
+from apache_beam.options.pipeline_options import PipelineOptions
 
 
-with beam.Pipeline() as pipeline:
-    
-    read_file = (pipeline | 'Read File' >> beam.io.ReadFromText('inputs/InfoDataflow', skip_header_lines = 1)
-                          | 'Divide' >> beam.Map(lambda x: (x.split(',')[0],float(x.split(',')[2])))
-                          | 'Count' >> beam.CombinePerKey(sum)
+class cast_to_date(beam.DoFn):
+    def __init__(self, formats):
+        self.formats = formats
+
+    def process(self,element):
+        for i in self.formats:
+            try:
+                element[i[0]] = (datetime.strptime(element[i[0]],i[1]).strftime('%Y-%m-%d'))
+            except:
+                element[i[0]] = ''
+        yield element
+
+
+def to_json(element):
+    return  {
+                'id': str(element[0]),
+                "amount" : element[1],
+                "process_time" :  int(time.mktime(datetime.utcnow().timetuple()))
+            }
+
+with beam.Pipeline() as pipe:
+
+    read_file = (pipe   | 'Read File' >> beam.io.ReadFromText('inputs/InfoDataflow', skip_header_lines = 1)
+                        | 'Split' >> beam.Map(lambda x: x.split(','))
                 )
-                          
 
-    print_data = read_file | 'Print' >> beam.Map(print)
+
+    conversions = read_file | 'Cast Dates' >> beam.ParDo(cast_to_date(formats = [[1,'%d-%m-%y']]))
+
+
+    group = (conversions | 'Group' >> beam.Map(lambda x: (x[0],float(x[2])))
+                         | 'Sum' >> beam.CombinePerKey(sum)
+            )
+
+    to_json_data = group | 'Convert to json' >> beam.Map(to_json)
+
+    print_data = to_json_data | beam.Map(print)
