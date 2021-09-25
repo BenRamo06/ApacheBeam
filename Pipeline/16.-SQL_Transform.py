67d156cd1eb293e5dfe488de543d5f1ca5b5634d
Benito Ramos <daniel.ramos06sql@gmail.com>
2021-09-25 01:41:56 -0500

    Add code SQL Transformation

---
+++ Pipeline/16.-SQL_Transform.py
@@ -0,0 +1,30 @@
+import typing
+import apache_beam as beam
+import logging
+import json
+from apache_beam.transforms.sql import SqlTransform
+
+
+def only_max(element, data):
+    if (element['id'],element['process']) in data:
+        return element
+
+Purchase = typing.NamedTuple('Purchase',
+                             [('id', int)])
+
+with beam.Pipeline() as pipe:
+
+    read_file = (pipe | 'Read file' >> beam.io.ReadFromText(file_pattern = 'inputs/jsonFile*')
+                      | 'Load json' >> beam.Map(json.loads))
+
+    max_value = (read_file  | 'Get Key/Value'   >> beam.Map(lambda x: (x['id'],x['process']))
+                            | 'Get max process by Key'  >> beam.CombinePerKey(max))
+
+    filter = read_file | 'Only max process' >> beam.Filter(only_max, data = beam.pvalue.AsIter(max_value))
+
+    row_data = filter | beam.Map(lambda x: beam.Row(id = int(x['id'])))
+    
+    sql_query = row_data  | SqlTransform(""" SELECT id FROM PCOLLECTION WHERE id >= 1""")
+
+    print_data = sql_query | beam.Map(print)
+
--- Pipeline/test.py
+++ Pipeline/test.py
@@ -1,14 +1,25 @@
 import apache_beam as beam
 
-with beam.Pipeline() as pipeline:
-  plants = (
-      pipeline
-      | 'Garden plants' >> beam.Create([
-          ('🍓', 'Strawberry'),
-          ('🥕', 'Carrot'),
-          ('🍆', 'Eggplant'),
-          ('🍅', 'Tomato'),
-          ('🥔', 'Potato'),
-      ])
-      | 'Key-Value swap' >> beam.KvSwap()
-      | beam.Map(print))
+def sum_l(l):                       
+    s0, s1 = 0, 0                                         
+    for i in range(len(l)):                                        
+        s0 += float(l[i][0])                                                      
+        s1 += float(l[i][1])                
+    return [s0, s1] 
+
+with beam.Pipeline() as p:
+    read =  (p  | 'Read Input' >> beam.Create([ 'name1,1,place1,2.,1.5',
+                                                'name1,1,place1,3.,0.5',
+                                                'name1,1,place2,1.,1',
+                                                'name1,2,place3,2.,1.5',
+                                                'name2,2,place3,3.,0.5'
+                                             ])
+                | 'Split Commas' >> beam.Map(lambda x: x.split(','))
+                | 'Prepare Keys' >> beam.Map(lambda x: (x[:-2], x[-2:]))
+                | 'Group Each Key' >> beam.GroupByKey()
+                | 'Make Summation' >> beam.Map(lambda x: [x[0], sum_l([e for e in x[1]])])
+                #| 'Write Results' >> beam.io.WriteToText('results.csv'))
+    )
+
+
+    print_data = read | 'Print' >> beam.Map(print)
