4ebb2891fef9394a3cfae55b7a407df31e9f216a
Benito Ramos <daniel.ramos06sql@gmail.com>
2022-03-02 18:05:33 -0600

    add prerequisites

--- Prerequisites/01.-Libraries.py
+++ Prerequisites/01.-Libraries.py
@@ -12,9 +12,34 @@ run('apt-get upgrade')
 # Install apache-beam 
 run('pip install apache-beam')
 
-# Install libraries to GCP
-#run('pip install apache-beam[gcp]')
-#run('pip install apache-beam[interactive]')
-#run('pip install google-apitools')
 
+
+## Install virtualenv package: 
+#pip3 install virtualenv
+## Create new environment:
+#python3.8 -m virtualenv env
+## Activate environment:
+#source env/bin/activate
+
+## Install libraries to GCP
 # pip3 install apache-beam[gcp]
+
+## Exit environment
+# deactivate
+## Remove environment
+# rm -rf env
+
+
+
+#-- Download version Python
+#wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz
+#-- unzip file
+#tar -xf Python-3.8.0.tgz
+#-- go to file
+#cd Python-3.8.0
+#-- install version python
+#./configure --enable-optimizations
+#make -j 8
+#sudo make altinstall
+#-- Check version python
+#python3.8 --version
