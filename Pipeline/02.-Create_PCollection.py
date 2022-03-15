f48276d64abfc18fddfff0f8691f3da207d85326
Benito Ramos <daniel.ramos06sql@gmail.com>
2022-03-14 18:20:58 -0600

    add Collection

--- Pipeline/02.-Create_PCollection.py
+++ Pipeline/02.-Create_PCollection.py
@@ -9,3 +9,10 @@ with beam.Pipeline() as pipeline:
                                                      [1,10.3,'MEX']])
               | 'Print'              >> beam.Map(print))
 
+
+
+
+# PCollection are Immutable : Apply a transformation on a PCollection it gives us a new PCollection
+# Element Type: PCollection must be of any type, but all must be the same type
+# Operation Type: it doesn't support grained operations. it means We can't apply transformations on specific element
+# Timestamp: Each element in a PColllection contains a timestamp UnBounded: Source assigns timestamp Bounded : Every element is set to same timestamp
