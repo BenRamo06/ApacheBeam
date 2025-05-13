ef178a65be6fd9db38168f94c80d7b85628973c9
BenRamo06 <90486154+BenRamo06@users.noreply.github.com>
2025-05-12 20:08:11 -0600

    Create parser.py

---
+++ Template/NameTuple/extract/parser.py
@@ -0,0 +1,17 @@
+from apache_beam import PTransform, PCollection, Map
+from apache_beam.io import ReadFromText
+from cnt_utils.parsers import row_to_array
+from typing import Callable
+
+
+class parse_files(PTransform):
+    def __init__(self, path: str, delimiter: str, parser: Callable):
+        self.path = path
+        self.delimiter = delimiter
+        self.parser = parser
+
+    def expand(self, pcoll: PCollection):
+        lines = pcoll | "ReadLines" >> ReadFromText(self.path)
+        parsed_data = lines | "SplitAndParse" >> Map(lambda row: self.parser(row_to_array(row, delimiter=self.delimiter)))
+
+        return parsed_data
